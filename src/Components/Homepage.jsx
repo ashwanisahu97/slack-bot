86e67d477df5f92c17dde69c5aedf8f471c0cecc
ashwanisahu97 <ashsahu627@gmail.com>
2022-09-02 14:55:50 +0530

    general section

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -2,27 +2,28 @@
 import { AtSignIcon, ChatIcon, ChevronDownIcon, EditIcon } from '@chakra-ui/icons'
 import { Box, Button, Flex, Heading,Text } from '@chakra-ui/react'
 import React from 'react'
-import 'font-awesome/css/font-awesome.min.css'; 
+import {fayoutube} from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
 import {
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
+  Wrap,
+  WrapItem
 } from '@chakra-ui/react'
 function Homepage() {
   return (
     <Box>
         <Flex>
           <Box bg={"#3f0e40"} color={ "#9f87a0 "} w={"350px"} border={"1px solid red"}>
-          <Box>
               <Flex gap={ "100px"}>
                   <Box p={ "15px"}><Heading as="h3" color={"white"} size={ "lg"}>FT WEB_17</Heading></Box>
                   <Box borderRadius={"50%"} p={"10px"} pt={ "0px"} bg={"white"} h={"50px"} w={"50px"} mt={ "15px"}>
                       <EditIcon h={"30px"} w={ "30px"} mt={ "10px"} />
                   </Box>
                   </Flex>
-        </Box>
           <Flex gap={"10px"}>
               <Box>
                   <ChatIcon w={"30px"} h={"30px"} mt={ "7px"} />
@@ -92,18 +93,35 @@ function Homepage() {
                   <Heading as="h3" size={ "lg"}> Direct Messages </Heading> 
               </Box>
               <Box>
-                  <Flex>
+                          <Flex gap={ "3"}  mb={ "10px"}>
                       <Box>
-                      <FontAwesomeIcon icon="fa-regular fa-user-plus" />
+                                  <Avatar size={ "sm"} name='Kola Tioluwani' src='https://bit.ly/tioluwani-kolawole' />
                       </Box>
-                      <Box></Box>
+                      <Box>
+                                  <Text fontStyle={"bold"}  fontSize={ "20px"}>Kola Tioluwani </Text>
+                      </Box>
+                </Flex>
+                          <Flex gap={ "3"}  mb={ "10px"}>
+                      <Box>
+                                  <Avatar size={ "sm"} name='Prosper Otemuyiwa' src='https://bit.ly/prosper-baba' />
+                      </Box>
+                      <Box>
+                                  <Text fontStyle={"bold"}  fontSize={ "20px"}>Prosper Otemuyiwa </Text>
+                      </Box>
+                </Flex>
+                          <Flex gap={"3"} mb={ "10px"}>
+                      <Box>
+                                  <Avatar size={ "sm"} name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+                      </Box>
+                      <Box>
+                                  <Text fontStyle={"bold"}  fontSize={ "20px"}>Segun Adebayo </Text>
+                      </Box>
                 </Flex>
               </Box>
           </Box>
    </Box>
-          <Box>
-            <Box>
-              <Flex>
+              <Box w={"1200px"} border={ "1px solid red"}>
+              <Flex border={"1px solid red"} justify={"space-between"}>
                 <Box>
           <Menu>
               <MenuButton fontSize={ "25px"} as={Button} rightIcon={<ChevronDownIcon />}>
@@ -118,19 +136,31 @@ function Homepage() {
   </MenuList>
                   </Menu>
                 </Box>
-                <Box>
+
                   <Box>
-                    <Flex>
-                      <Box></Box>
-                      <Box></Box>
+                          <Flex borderRadius={"10px"} border={"1px solid grey"} p={ "2px"}>
+                              <Box p={ "2px"}><Wrap fontSize={ "16px"}>
+  <WrapItem>
+    <Avatar size='xs' name='Prosper Otemuyiwa' src='https://bit.ly/prosper-baba' />
+  </WrapItem>
+  <WrapItem>
+    <Avatar size='xs' name='Christian Nwamba' src='https://bit.ly/code-beast' />
+  </WrapItem>
+  <WrapItem>
+    <Avatar size='xs' name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+  </WrapItem>
+</Wrap></Box>
+                                      <Box borderRadius={"5px"} border={"1px solid grey"} p={ "2px"}>
+                                         <Text>206</Text>
+                      </Box>
+                      <Box borderRadius={"5px"} border={"1px solid grey"} p={ "2px"}><FontAwesomeIcon icon="fa-brands fa-youtube" /></Box>
                     </Flex>
-                  </Box>
+                      </Box>
+                      </Flex>
                 </Box>
                 </Flex>
-              </Box>
-          </Box>
-        </Flex>
-      </Box>
+      </Box >
+            
   );
 }
 
