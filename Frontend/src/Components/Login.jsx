ff96eb18908d2f7478b932476c8960dc45bc4238
Ashwini Prakash <100183151+Ashwiniprakash007@users.noreply.github.com>
2022-09-02 15:20:50 +0530

    Login page

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -1,1 +1,46 @@
+import React, { useState } from "react";
 
+const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePaasordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleSubmit = async () => {
+        const payload ={
+            email,
+            password
+        }
+        const response = await fetch("http://localhost:8080/user/login", {
+            method : "POST",
+            body : JSON.stringify(payload),
+            headers: {
+                "Content-Type" : "application/json"
+            }, 
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            localStorage.setItem("token", res.token)
+        })
+        .catch((err) => console.log(err))
+        // console.log(response)
+    }
+    return(
+        <div>
+            <br/>
+            <input type="email" placeholder="email" value={email} onChange={handleEmailChange}></input> 
+            <br/>
+            <input type="password" placeholder="password" value={password} onChange={handlePaasordChange}></input>
+            <br/>
+            <button onClick={handleSubmit}>Login</button>
+        </div>
+    )
+}
+
+export  {Login};
